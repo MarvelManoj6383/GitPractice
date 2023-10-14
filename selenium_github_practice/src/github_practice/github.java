133b25efa42414e7f36e56069d4271b9aabe5d26
NEW <NEW@DESKTOP-A6PJ4O8>
2023-10-14 09:44:35 +0530

    1st

--- selenium_github_practice/src/github_practice/github.java
+++ selenium_github_practice/src/github_practice/github.java
@@ -1,9 +1,15 @@
 package github_practice;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class github {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://www.instagram.com");
+		Thread.sleep(3000);
 
 	}
 
